d2e8c090de1b3f5a6d497ace50bd65a5cb1b31de
Peter Souter <petems@users.noreply.github.com>
2017-04-10 11:39:23 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -29,7 +29,5 @@ node default {
   # example code for the classroom
   include examples::puppetize
   
-  notify { "This is the default message from the production environment": }
-  
-  notify { "Changed in Github again": }
+  notify {"Hello world! I am ${::fqdn}":}
 }

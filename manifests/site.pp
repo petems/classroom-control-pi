f29c89ec4d8bfeb245f9099489aa2926a43dd051
hemakumar8 <hemakumar8@gmail.com>
2017-04-10 19:30:10 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -21,6 +21,20 @@
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
 
+node hemakumar8.puppetlabs.vm {
+  # This is where you can declare classes for all nodes.
+  # Example:
+  #   class { 'my_class': }
+  
+  # example code for the classroom
+  include examples::puppetize
+  
+  notify { "This is the default message from the production environment": }
+  
+  #notify { "Hello, change in Github": }
+  notify { "This will only be enforced on the Linux contain": }
+}
+
 node default {
   # This is where you can declare classes for all nodes.
   # Example:

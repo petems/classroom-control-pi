58ff7b34d7edf23c7e5cb0618547410c02cc4ed2
Kishore Thakur <kishorethakur.rajput@gmail.com>
2017-04-11 20:20:45 +0530

    added require package statement

--- site/motd/manifests/init.pp
+++ site/motd/manifests/init.pp
@@ -2,6 +2,7 @@ class motd {
   exec { "figlet 'Welcome to ${::fqdn}!' > /etc/motd":
     path    => '/bin:/usr/bin:/usr/local/bin',
     creates => '/etc/motd',
+    require => Package['figlet'],
     # what relationship should we add here to ensure that figlet is available to run?
     
   }

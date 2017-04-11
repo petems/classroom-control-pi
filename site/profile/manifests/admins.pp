ac9706f398fe07f0f1e01dce953f774fb813040d
gdunk <hisrocketsignite@hotmail.com>
2017-04-11 23:28:06 +0800

    Update admins.pp

--- site/profile/manifests/admins.pp
+++ site/profile/manifests/admins.pp
@@ -9,6 +9,6 @@ class profile::admins {
   # manage several instances of our defined type
   skeleton::managed_user { ['jose', 'alice', 'bob']:
     password => $password,
-    home  => "/var/tmp/${title}",
+    home  => "/var/tmp/${name}",
   }
 }

a2f309591d2358932a7189752850a5545d5805c7
hemakumar8 <hemakumar8@gmail.com>
2017-04-11 15:47:43 +0530

    removed node specific

--- manifests/site.pp
+++ manifests/site.pp
@@ -21,18 +21,7 @@
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
 
-node hemakumar8.puppetlabs.vm {
-  # This is where you can declare classes for all nodes.
-  # Example:
-  #   class { 'my_class': }
-  
-  # example code for the classroom
-  include examples::puppetize
-  
-  notify { "This will only be enforced on the Linux contain": }
-  
-  #notify { "Hello, change in Github": }
-}
+
 
 node default {
   # This is where you can declare classes for all nodes.

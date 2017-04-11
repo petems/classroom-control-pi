fd23693833fbc7b41fe61efaef7c5940ceb05d5f
gdunk <hisrocketsignite@hotmail.com>
2017-04-11 21:12:22 +0800

    Update redis.pp

--- site/profile/manifests/redis.pp
+++ site/profile/manifests/redis.pp
@@ -1,5 +1,5 @@
 class profile::redis ( 
-$master = false
+$master = false,
 ) {
   # We'll require our epel class so that it is enforced first.
   # Don't forget to update that class before enforcing your code
@@ -19,3 +19,4 @@ $master = false
     slaveof => $slaveof,
   }
 }
+

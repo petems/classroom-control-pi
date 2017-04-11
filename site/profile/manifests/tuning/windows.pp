a1904154e48586d59cf1ac608f5a4f5b1ed37844
Kunal <mahi.kunal@gmail.com>
2017-04-11 19:33:25 +0530

    Update windows.pp

--- site/profile/manifests/tuning/windows.pp
+++ site/profile/manifests/tuning/windows.pp
@@ -1,18 +1,19 @@
+#
 class profile::tuning::windows {
-    registry::value { 'KeepAliveInterval':
-      key    => 'HKLM\SYSTEM\CurrentControlSet\Services\AFD\Parameters',
-      value  => 'KeepAliveInterval',
-      data   => '1',
-    }
-    registry::value { 'TcpMaxDataRetransmissions':
-      key    => 'HKLM\SYSTEM\CurrentControlSet\Services\TCPIP\Parameters',
-      value  => 'TcpMaxDataRetransmissions',
-      data   => '5',
-    }
-    registry::value { 'TcpTimedWaitDelay':
-      key    => 'HKLM\SYSTEM\CurrentControlSet\Services\TCPIP\Parameters',
-      value  => 'TcpTimedWaitDelay',
-      data   => '0x0000001e',
-    }
+  registry::value { 'KeepAliveInterval':
+    key   => 'HKLM\SYSTEM\CurrentControlSet\Services\AFD\Parameters',
+    value => 'KeepAliveInterval',
+    data  => '1',
+  }
+  registry::value { 'TcpMaxDataRetransmissions':
+    key   => 'HKLM\SYSTEM\CurrentControlSet\Services\TCPIP\Parameters',
+    value => 'TcpMaxDataRetransmissions',
+    data  => '5',
+  }
+  registry::value { 'TcpTimedWaitDelay':
+    key   => 'HKLM\SYSTEM\CurrentControlSet\Services\TCPIP\Parameters',
+    value => 'TcpTimedWaitDelay',
+    data  => '0x0000001e',
+  }
 
 }

2ee10799d5408f25e13131ae71924ec6d3b2f7ad
Peter Souter <petems@users.noreply.github.com>
2017-04-11 13:29:52 +0100

    Add skel setup

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,12 +1,16 @@
 class skeleton {
   file { '/etc/skel':
-#    ensure => ???,   # what value should go here?
+    ensure => 'directory',
     owner  => 'root',
     group  => 'root',
     mode   => '0755',
   }
   
-  # add a resource to manage /etc/skel/.bashrc
-
+  file { '/etc/skel/.bashrc':
+    ensure => 'file',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+  }
 
 }

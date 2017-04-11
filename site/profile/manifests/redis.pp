8b2cd8a69dcf45279ff98f1205fa1fe1fcd64117
Kishore Thakur <kishorethakur.rajput@gmail.com>
2017-04-11 18:34:36 +0530

    Updated Redis master/slaveof details

--- site/profile/manifests/redis.pp
+++ site/profile/manifests/redis.pp
@@ -1,11 +1,19 @@
 class profile::redis {
+ $master = false 
+ }
+ {
   # We'll require our epel class so that it is enforced first.
   # Don't forget to update that class before enforcing your code
   require profile::epel
+  if $master{ $slaveof = undef
+  }
+  else {
+    $slaveof = 'master.puppetlabs.vm 6479'
+    }
   
   class { 'redis':
     # what parameter should we pass to set maxmemory to 10mb?
     maxmemory => '10mb'
+    slaveof = $slaveof,
   }
   
-}

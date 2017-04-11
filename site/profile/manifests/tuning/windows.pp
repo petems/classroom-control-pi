e8d5487da62aaad15d5c71cb52011e449bf47699
sor609 <sor609@users.noreply.github.com>
2017-04-11 22:01:01 +0800

    Update windows.pp

--- site/profile/manifests/tuning/windows.pp
+++ site/profile/manifests/tuning/windows.pp
@@ -6,5 +6,21 @@ class profile::tuning::windows {
     }
 
   # Add more registry resources as directed
+  registry::value { 'KeepAliveInterval':
+    key => 'HKLM\SYSTEM\CurrentControlSet\Services\AFD\Parameters',
+    value => 'KeepAliveInterval',
+    data => '1',
+  }
+
+  registry::value { 'TcpMaxDataRetransmission':
+    key => 'HKLM\SYSTEM\CurrentControlSet\Services\TCPIP\Parameters',
+    value => 'TcpMaxDataRetransmissions',
+    data => '5',
+  }
   
+  registry::value { 'TcpTimedWaitDelay':
+    key => 'HKLM\SYSTEM\CurrentControlSet\Services\TCPIP\Parameters',
+    value => 'TcpTimedWaitDelay',
+    data => '0x0000001e',
+  }
 }

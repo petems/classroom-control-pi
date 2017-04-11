b9c64dd4256d3ff46234058e041ea75c9690812f
hemakumar8 <hemakumar8@gmail.com>
2017-04-11 19:43:34 +0530

    added registry values

--- site/profile/manifests/tuning/windows.pp
+++ site/profile/manifests/tuning/windows.pp
@@ -5,6 +5,15 @@ class profile::tuning::windows {
       data   => '1',
     }
 
-  # Add more registry resources as directed
+    registry::value { 'TcpMaxDataRetransmission':      
+       key    => 'HKLM\SYSTEM\CurrentControlSet\Services\TCPIP\Parameters',      
+       value  => 'TcpMaxDataRetransmissions',      
+       data   => '5',    
+    }
+    registry::value { 'TcpTimedWaitDelay':      
+       key    => 'HKLM\SYSTEM\CurrentControlSet\Services\TCPIP\Parameters',      
+       value  => 'TcpTimedWaitDelay',      
+       data   => '0x0000001e',    
+    }
   
 }

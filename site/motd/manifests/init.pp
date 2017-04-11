9a1597ee53d018577ed777f57898478cbc7ea9a3
Peter Souter <petems@users.noreply.github.com>
2017-04-11 16:00:03 +0100

    Adds relationship

--- site/motd/manifests/init.pp
+++ site/motd/manifests/init.pp
@@ -2,8 +2,7 @@ class motd {
   exec { "figlet 'Welcome to ${::fqdn}!' > /etc/motd":
     path    => '/bin:/usr/bin:/usr/local/bin',
     creates => '/etc/motd',
-    # what relationship should we add here to ensure that figlet is available to run?
-    
+    require => Package['figlet'],
   }
 
   package { 'figlet':

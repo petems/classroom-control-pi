f6d108eade391b90740404edb51a9c676a179faa
gdunk <hisrocketsignite@hotmail.com>
2017-04-11 20:27:32 +0800

    add bashrc

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,11 +1,18 @@
 class skeleton {
   file { '/etc/skel':
-#    ensure => ???,   # what value should go here?
+    ensure => directory,   # what value should go here?
     owner  => 'root',
     group  => 'root',
     mode   => '0755',
   }
   
+  file { '/etc/skel/.bashrc':
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    source => 'puppet:///modules/skeleton/.bashrc',
+  }
+    
   # add a resource to manage /etc/skel/.bashrc
 
 

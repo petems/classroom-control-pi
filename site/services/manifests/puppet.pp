a505af2cc51c98a2e1a029fe117988d23b944452
Peter Souter <petems@users.noreply.github.com>
2017-04-12 10:09:34 +0100

    Update puppet.pp

--- site/services/manifests/puppet.pp
+++ site/services/manifests/puppet.pp
@@ -2,6 +2,7 @@ class services::puppet {
   # ensure the puppet service doesn't interfere with our testing
   # TODO: Add parameters to ensure that this service is not running
   service { 'puppet':
-
+    ensure => 'stopped',
+    enable => 'false',
   }
 }

b59e463a024c46d0b46355f53887c52b580d8572
hemakumar8 <hemakumar8@gmail.com>
2017-04-12 14:50:01 +0530

    enabled

--- site/services/manifests/puppet.pp
+++ site/services/manifests/puppet.pp
@@ -1,7 +1,7 @@
 class services::puppet {
   # ensure the puppet service doesn't interfere with our testing
    service { 'puppet':
-     ensure => stopped,
-     enable => false,
+     ensure => running,
+     enable => true,
   }
 }
